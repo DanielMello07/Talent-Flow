4f26fb6bbf613bc4208e7e77222f3c5e950f5bf0
DaniMSM <danmarmel07@gmail.com>
2025-10-30 14:32:07 -0300

    Estrutura inicial do Talent Flow, com spring

---
+++ src/main/java/com/example/TalentFlow/TalentFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.TalentFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TalentFlow/TalentFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TalentFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
