6c9e23df9595a6ec1f4364c6125c6a8a07590fb6
LucineyJS <lucineyjesus.s@gmail.com>
2025-11-05 13:39:34 -0300

    :hammer: refactor: Corrige ‘classe repositoryUsuario’ para ‘interface UsuarioRepository’

---
+++ src/main/java/com/example/TalentFlow/repository/UsuarioRepository.java
@@ -0,0 +1,6 @@
+package com.example.TalentFlow.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<com.example.TalentFlow.model.Usuario, Long>{
+}
--- src/main/java/com/example/TalentFlow/repository/repositoryUsuario.java
+++
@@ -1,8 +0,0 @@
-package com.example.TalentFlow.repository;
-
-import com.example.TalentFlow.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface RepositoryUsuario extends JpaRepository<Usuario, Long>{
-}
