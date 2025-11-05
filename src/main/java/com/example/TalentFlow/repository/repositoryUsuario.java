02b18abbbce4342b90204c485ecf34a67b214e43
LucineyJS <lucineyjesus.s@gmail.com>
2025-11-05 10:13:46 -0300

    :sparkles: feat: Cria Repository Usuario

--- src/main/java/com/example/TalentFlow/repository/repositoryUsuario.java
+++ src/main/java/com/example/TalentFlow/repository/repositoryUsuario.java
@@ -1,4 +1,8 @@
 package com.example.TalentFlow.repository;
 
-public class repositoryUsuario {
+import com.example.TalentFlow.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface RepositoryUsuario extends JpaRepository<Usuario, Long>{
 }
